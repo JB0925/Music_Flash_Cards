6fd271d1b81f583ae4b42db0117d39bca5a74174
JnyJny <erik.oshaughnessy@gmail.com>
2020-11-09 11:51:36 -0600

    Big Poetry re-organization

--- music_flash_cards/music_note_flash_cards.py
+++ music_flash_cards/music_note_flash_cards.py
@@ -1,98 +1,100 @@
-from pyaudio_test import read_and_write_audio, get_pitch
-import tkinter as tk
-from pil import Image, ImageTk
-import random
-import os
-import time
+"""Music Flash Cards
 
-"""This module creates the flashcards used to run the flashcard
+This module creates the flashcards used to run the flashcard
 app. It also runs the main loop, which will need to be exited by 
-the user when they are ready to stop."""
+the user when they are ready to stop.
+"""
+
+import os
+import random
+import time
+import tkinter as tk
+
+from PIL import Image, ImageTk
+from pyaudio_test import read_and_write_audio, get_pitch
+
+from .cards import note_card_paths, incorrect_card_path, correct_card_path
 
 
-def display_note_flashcard():
+def display_note_flashcard() -> None:
     """This function is much like the two below it, so this docstring and
     the comments can serve for those two as well. The reason for the other two functions
     is that this function pulls a random card from the file and, to prevent
     it from pulling the Correct or Incorrect cards, they have been put into
     a separate file."""
 
-    root = tk.Tk()                  #create the main Tkinter widget
-    folder = 'C:/Users/superuser/Music_Flash_Cards/chromatic_note_cards'
-    filename = random.choice(os.listdir(folder))
-    note = os.path.splitext(filename)[0]                #get the note name from the flash card
-    
-    img = Image.open(os.path.join(folder, filename))        #open the file with the image
-    photo = ImageTk.PhotoImage(img)                         #load the image
-    canvas = tk.Canvas(root, width=400, height=400)         #place it on a Tkinter Canvas object
+    root = tk.Tk()
+
+    filename = random.choice(note_card_paths())
+    note = os.path.splitext(filename)[0]
+
+    img = Image.open(filename)
+    photo = ImageTk.PhotoImage(img)
+    canvas = tk.Canvas(root, width=400, height=400)
     canvas.pack()
-    canvas.create_image(50,50, anchor='nw', image=photo)
-    
-    root.after(10000, root.destroy)                         #stop the card from running after 10 seconds
+    canvas.create_image(50, 50, anchor="nw", image=photo)
+
+    root.after(10_000, root.destroy)
     root.mainloop()
 
     return note
 
 
-def display_correct_card():
+def display_correct_card() -> None:
     card = tk.Tk()
 
-    folder = 'C:/Users/superuser/Music_Flash_Cards/correct_and_incorrect_cards'
-    correct_card = 'Correct.PNG'
-    
-    img = Image.open(os.path.join(folder, correct_card))
+    img = Image.open(correct_card_path())
+
     photo = ImageTk.PhotoImage(img)
     canvas = tk.Canvas(card, width=400, height=400)
     canvas.pack()
-    canvas.create_image(50,50, anchor='nw', image=photo)
+    canvas.create_image(50, 50, anchor="nw", image=photo)
 
     card.after(5000, card.destroy)
     card.mainloop()
 
 
-def display_incorrect_card():
+def display_incorrect_card() -> None:
     card = tk.Tk()
 
-    folder = 'C:/Users/superuser/Music_Flash_Cards/correct_and_incorrect_cards'
-    incorrect_card = 'Incorrect.PNG'
-    
-    img = Image.open(os.path.join(folder, incorrect_card))
+    img = Image.open(incorrect_card_path())
+
     photo = ImageTk.PhotoImage(img)
     canvas = tk.Canvas(card, width=400, height=400)
     canvas.pack()
-    canvas.create_image(50,50, anchor='nw', image=photo)
+    canvas.create_image(50, 50, anchor="nw", image=photo)
 
-    card.after(5000, card.destroy)
+    card.after(5_000, card.destroy)
     card.mainloop()
-
 
 
 def main():
     """Function that runs the main loop. This contains the
     audio functions as well. Audio is recorded and then the
-    frequency of the audio is used to get the note name. That 
+    frequency of the audio is used to get the note name. That
     note name is then compared to the note name on the card and,
-    if they are the same, the correct card will appear. If not, 
+    if they are the same, the correct card will appear. If not,
     the incorrect card appears. Each of these events takes
     advantage of a delay to allow for time to record and time
     for the user to mentally process and find the note given on
     each card."""
 
-    while True:                             #user will need to stop the program manually
-        x = display_note_flashcard()        #show the flashcard
-        time.sleep(5)                       #allow for time to process and find the note
-        sound = read_and_write_audio()      #record audio
-        p = get_pitch(sound)                #get the note name from the recorded sound
-    
-        if p == x:                          #if the value of the recorded sound is equal
-            display_correct_card()          #to the name of the note on the card, display
-            time.sleep(5)                   #the "Correct!" card. If not, display the
-        else:                               #"Incorrect..." card.
+    while True:
+
+        expected_pitch = display_note_flashcard()
+        time.sleep(5)
+
+        sound = read_and_write_audio()
+
+        played_pitch = get_pitch(sound)
+
+        if played_pitch == expected_pitch:
+            display_correct_card()
+        else:
             display_incorrect_card()
-            time.sleep(5)
+
+        time.sleep(5)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
-
